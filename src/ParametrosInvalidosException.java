cf93e745d1fd926f49cf4cbbde7bdb64e1d49593
Caetano Copat <caetanocopat@gmail.com>
2023-10-06 00:52:00 -0300

    added some more exceptions cases to study purpouses

--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,6 +1,26 @@
 public class ParametrosInvalidosException extends Exception {
 
+
+    //First constructor
+
     public ParametrosInvalidosException(String message){
         super (message);
+    }
+
+    // 'null' constructor
+    public ParametrosInvalidosException(){
+
+    }
+
+    // Throwable constructor
+    // If the custom exception can be caused by some other exception. You can pass in the Throwable that actually caused it.
+    public ParametrosInvalidosException(Throwable cause){
+        super(cause);
+    }
+
+    // Throwable + message constructor
+
+    public ParametrosInvalidosException(String message, Throwable cause){
+        super(message, cause);
     }
 }
