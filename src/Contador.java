56e9cac4a4aa24c314babd8d9db529c3b7e6caaf
Caetano Copat <caetanocopat@gmail.com>
2023-10-05 21:27:51 -0300

    Project commit

---
+++ src/Contador.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+
+        Scanner scannerContador = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número:");
+        int numeroUm = scannerContador.nextInt();
+
+        System.out.println("Digite o segundo número:");
+        int numeroDois = scannerContador.nextInt();
+
+        try {
+            contar(numeroUm,numeroDois);    
+        } catch (ParametrosInvalidosException pie) {
+            // pie.printStackTrace();
+            System.out.println("O segundo numero deve ser maior do que o primeiro.");
+        };
+
+        scannerContador.close();
+
+
+    }
+
+    static void contar (int numero1, int numero2) throws ParametrosInvalidosException{
+
+        if (numero2 <= numero1){
+            throw new ParametrosInvalidosException();
+        }
+            
+        for (int i = 1; i <= (numero2-numero1); i++){
+            System.out.println("Imprimindo o número "+i);
+        }
+
+
+    }
+    
+}
